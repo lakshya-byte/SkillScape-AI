c8ef593bbbdf4433a123fcfeaa7f5fddbaedbe2d
lakshya-byte <lakshyadubey73@gmail.com>
2026-02-20 10:45:16 +0530

    import error

--- server/socket.js
+++ server/socket.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "./models/User.model.js";
+import { User } from "./models/user.model.js";
 import { Chat } from "./models/Chat.model.js";
 import { Message } from "./models/Message.model.js";
 
